4988fb25fb28ba5bb0cd923926ed4068dd8dd148
Erica Marigold <hi@devcomp.xyz>
2023-07-13 21:13:29 +0530

    fix: remove commit and push steps

--- flightjs/rebranding.sh
+++ flightjs/rebranding.sh
@@ -5,7 +5,3 @@ grep "@flightjs" pnpm -lr | xargs sed -i 's/@flightjs/@pnpm/g'
 mv pnpm/src/pnpm.ts pnpm/src/flightjs.ts
 
 grep "https://github.com/flightjs/flightjs" pnpm -lr | xargs sed -i 's/https:\/\/github.com\/flightjs\/flightjs/https:\/\/github.com\/flightpkg\/javascript/g'
-
-git add -A
-git commit -m "chore(ci): apply flightjs branding" --no-verify
-git push --set-upstream https://github.com/flightpkg/javascript.git main --force
