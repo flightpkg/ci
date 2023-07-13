59163d0eed635bd3455009be7864d59d50a814a7
Erica Marigold <hi@devcomp.xyz>
2023-07-13 23:11:49 +0530

    fix: command to `pg` link globally

--- flightjs/build.sh
+++ flightjs/build.sh
@@ -1,2 +1,2 @@
 curl -f https://get.pnpm.io/v6.16.js | node - add --global @pnpm/exe
-pnpm i --no-frozen-lockfile && pnpm run compile
+pnpm i --no-frozen-lockfile && pnpm link --dir ./pnpm/dev -g && pnpm run compile
